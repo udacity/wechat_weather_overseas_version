65faa22171b28e1fc7cdd05c26894fc46662f019
hank <wang.zuowen@me.com>
2019-04-15 12:39:06 +0800

    5-5

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,1 +1,48 @@
-Page({})
+const dayMap = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+Page({
+  date: {
+    weekWeather: []
+  },
+  onLoad() {
+    this.getWeekWeather()
+  },
+  onPullDownRefresh() {
+    this.getWeekWeather(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+  getWeekWeather(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/future',
+      data: {
+        time: new Date().getTime(),
+        city: "newyork"
+      },
+      success: res => {
+        let result = res.data.result
+        this.setWeekWeather(result)
+      },
+      complete: ()=>{
+        callback && callback()
+      }
+    })
+  },
+  setWeekWeather(result){
+    let weekWeather = []
+    for (let i=0; i<7; i++){
+      let date = new Date()
+      date.setDate(date.getDate() + i)
+      weekWeather.push({
+        day: dayMap[date.getDay()],
+        date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+        temp: `${result[i].minTemp}° - ${result[i].maxTemp}°`,
+        iconPath: '/images/' + result[i].weather + '-icon.png'
+      })
+    }
+    weekWeather[0].day = 'Today'
+    this.setData({
+      weekWeather:weekWeather
+    })
+  }
+})
